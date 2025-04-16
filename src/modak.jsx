025826ff674e1d2ed38fbc8901534a99cd6dd978
Surve <surve8729@gmail.com>
2025-04-16 15:09:56 +0530

    Add image in Modak component

--- src/modak.jsx
+++ src/modak.jsx
@@ -1,6 +1,7 @@
 export default function Modak() {
   return (
     <>
+      <img src="https://pipingpotcurry.com/wp-content/uploads/2022/08/Ukadiche-Modak-Piping-Pot-Curry-720x720.jpg" alt="modak-image"/>
       <h1>Modak</h1>
       <p>
         A modak is a traditional Indian sweet dumpling, especially popular
