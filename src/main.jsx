281cb67a534e14a30c1e8460d10be83c79e8d337
Surve <surve8729@gmail.com>
2025-04-16 15:05:42 +0530

    Make Modak component

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Greeting from "./Greeting.jsx";
+import Modak from "./modak.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Greeting />
+    <Modak />
   </StrictMode>,
 );
---
+++ src/modak.jsx
@@ -0,0 +1,11 @@
+export default function Modak() {
+  return (
+    <>
+      <h1>Modak</h1>
+      <p>
+        A modak is a traditional Indian sweet dumpling, especially popular
+        during the Ganesh Chaturthi festival.
+      </p>
+    </>
+  );
+}
